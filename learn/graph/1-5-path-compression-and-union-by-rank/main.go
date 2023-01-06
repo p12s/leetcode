39b692e5c0bb71760a9648891ca79893d975a70b
p12s <working-tam@yandex.com>
2023-01-06 04:22:32 +0300

    Added optimized path compression & union by rank example

---
+++ learn/graph/1-5-path-compression-and-union-by-rank/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func find(x int) int { // O(N)
+	if x == roots[x] {
+		return x
+	}
+	roots[x] = find(roots[x])
+
+	return roots[x]
+}
+
+func union(x, y int) { // O(N)
+	rootX := find(x)
+	rootY := find(y)
+	if rootX != rootY {
+		if rank[rootX] > rank[rootY] {
+			roots[rootY] = rootX
+		} else if rank[rootX] < rank[rootY] {
+			roots[rootX] = rootY
+		} else {
+			roots[rootY] = rootX
+			rank[rootX] += 1
+		}
+	}
+}
+
+func connected(x, y int) bool { // O(N)
+	return find(x) == find(y)
+}
+
+var (
+	size  = 10
+	roots = make([]int, size)
+	rank  = make([]int, size)
+)
+
+func main() {
+
+	// 1-2-5-6-7 3-8-9 4
+	for i := 0; i < size; i++ { // O(N)
+		roots[i] = i
+		rank[i] = 1
+	}
+
+	union(1, 2)
+	union(2, 5)
+	union(5, 6)
+	union(6, 7)
+	union(3, 8)
+	union(8, 9)
+
+	fmt.Println(connected(1, 5)) // true
+	fmt.Println(connected(5, 7)) // true
+	fmt.Println(connected(4, 9)) // false
+
+	// 1-2-5-6-7 3-8-9-4
+	union(9, 4)
+	fmt.Println(connected(4, 9)) // true
+}
--- learn/graph/README.md
+++ learn/graph/README.md
@@ -20,6 +20,8 @@
 |-----------------|------------------------|-----------|-----------|--------------|
 | Time Complexity | O(N)                   | O(logN)   | O(logN)   | O(logN)      |
 
-
-
+## Disjoint set optimized path compression & union by rank
+|                 | Union-find Constructor | Find      | Union     | Connected    |
+|-----------------|------------------------|-----------|-----------|--------------|
+| Time Complexity | O(N)                   | O(α(N))   | O(α(N))   | O(α(N))      |
 
